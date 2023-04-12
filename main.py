1e4b5b48eb8bdc213e129e7f969e010989b0c19e
reginaongowarsito <regina.ongowarsito@gmail.com>
2023-04-11 19:31:10 -0700

    First version - attempt to collect top 1000 of every year from 1900 to 2023

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import json
+import time
 
 
 '''
@@ -9,19 +11,53 @@ export SPOTIPY_CLIENT_ID=d44c1b8a65604265a5abc858294aa830
 export SPOTIPY_CLIENT_SECRET=09ff84f3e5774851858751621647985e
 export SPOTIPY_REDIRECT_URI=https://localhost:8888/callback
 '''
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 def main():
-	birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-	spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-	results = spotify.artist_albums(birdy_uri, album_type='album')
-	albums = results['items']
-	while results['next']:
-	    results = spotify.next(results)
-	    albums.extend(results['items'])
+	total_result = dict() # id to artist
+	
+	for year in range(2023, 1900, -1):
+		queryForYear(year, total_result)
+		# time.sleep(60)
 
-	for album in albums:
-	    print(album['name'])
+		print("Total result so far: " + str(len(total_result)))
+
+	json_object = json.dumps(total_result)
+
+	with open("results.json", "w") as outfile:
+		outfile.write(json_object)
+
+def queryForYear(year, total_result):
+	print("Fetching for year: " + str(year))
+	offset = 0
+	limit = 50
+	valid = True
+	while valid and offset < 1000:
+		try:
+			query_result = spotify.search("year:"+str(year), limit=limit, offset=offset, type='artist', market='US')
+			
+			for item in query_result['artists']['items']:
+				artist = simplifyArtistItem(item)
+				total_result[artist['id']] = artist
+
+			offset += limit
+		except spotipy.SpotifyException:
+			print("Query maximum offset hit at: " + str(offset))
+			valid = False
+
+	return total_result
+	
+	
+def simplifyArtistItem(item):
+	result = dict()
+	result['id'] = item['id']
+	result['name'] = item['name']
+	result['genres'] = item['genres']
+	result['popularity'] = item['popularity']
+	return result
+
+
 
 if __name__ == '__main__':
     main()
